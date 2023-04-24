63f4689634486cadab30777a52bd64326a56dc4f
ognobody <131667708+ognobody@users.noreply.github.com>
2023-04-24 03:59:14 -0700

    Create pr.py

---
+++ pr.py
@@ -0,0 +1,10 @@
+1=int(input("enter the frist number"))
+n2=float(input("enter the second number"))
+n3=input("enter third value")
+n4=10+2j
+n5=True
+print("the first value is",n1,type(n1))
+print("the second value is",n2,type(n2))
+print("the third value is",n3,type(n3))
+print(type(n4))
+print(type(n5))
